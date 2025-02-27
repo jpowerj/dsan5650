7ef7b8f194143876b7a46ddc5717cc877447fb1b
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-02-27 04:35:00 -0500

    Last updated in sidebar

--- _pre-set-updated.py
+++ _pre-set-updated.py
@@ -5,7 +5,7 @@ if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
   exit()
 
 cur_ts = str(datetime.datetime.now().strftime("%Y-%m-%d, %I:%M:%S %p"))
-env_line = f'DSAN5450_LAST_UPDATED="{cur_ts}"'
+env_line = f'DSAN5650_LAST_UPDATED="{cur_ts}"'
 with open("_environment", 'w', encoding='utf-8') as outfile:
   outfile.write(env_line)
 
