a4624d318505749e854daf7c12c0020180ecad21
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2026-07-29 03:37:01 -0400

    dsan-globals as submodule

--- dsan-globals-v0/_pw_protect.py
+++
@@ -1,7 +0,0 @@
-import shutil
-
-src_fpath = "./_index-template.html"
-target_fpath = "./docs/index.html"
-shutil.copyfile(src_fpath, target_fpath)
-print(f"Password-protection index copied to {target_fpath}")
-
